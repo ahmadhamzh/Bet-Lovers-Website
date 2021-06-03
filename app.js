079a4f932bf4a8859937924027c09f2795fffdd1
ahmadhamzh1 <>
2021-06-03 15:32:51 +0000

    add loop command

---
+++ app.js
@@ -0,0 +1,47 @@
+
+var x = "if you don't like pets don't enter"
+var c = "you'll finde all you need about cats welcom to our site"
+var d = "you'll finde all you need about dogs welcom to ou site"
+var b = "you'll finde all you need about birds welcom to our site"
+
+alert(x)
+
+var welcom = "welcom " + name
+var y = prompt ("what's your favorite pets")
+ 
+
+ if(y ==="cats")
+ {alert(c)}
+ else if (y ==="dogs")
+ {alert(d)}
+else if (y ==="birds")
+{alert (b)}
+else {alert ("welcom to our site")}
+
+var SecretNumber = 6
+var GuessAnswer = prompt("Between 1 to 9 gusse the secrete Number you got three gusses")
+let i = 0
+
+while(GuessAnswer !=6 || i == 3 ){
+
+ 
+i++
+
+  GuessAnswer = prompt ("not Right try Again")
+
+  
+}
+
+if (GuessAnswer = 6){
+
+alert("great guess")
+}
+ Rate = prompt("pleas rate our site")
+
+for (var a= 0; a < Rate;){
+
+ document .write(' <img src="https://media.istockphoto.com/photos/golden-star-award-for-game-isolated-on-white-background-star-star-3d-picture-id1173610414"width="30"height="30"/>' )
+++a
+}
+
+
